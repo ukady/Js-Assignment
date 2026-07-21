a7750dd4d4b39a54f52320d8fc2ef0bb21b727c0
ukady <ukamakafaith137@gmail.com>
2026-07-21 16:30:08 +0100

    precious

---
+++ script.js
@@ -0,0 +1,55 @@
+let age = 20;
+if (age >= 18){
+    console.log("Eligible to vote");
+}else{
+    console.log("Not eligible to vote");
+}
+
+
+let score = 7;
+
+if (score >= 70 && score <= 100){
+    console.log("A");
+}else if (score >= 60 && score <= 69){
+    console.log("B");
+}else if (score >= 50 && score <= 59){
+    console.log("C");
+}else {
+       console.log("F")
+}
+
+let num1 = 2;
+let num2 = 3;
+if (num1 > num2){
+    console.log("num1 + is the largest")
+}else if (num2 >  num1){
+    console.log("num2 + is the largest")
+}else{
+    console.log("Both are equal")
+}
+
+let username = "admin";
+let password = "1234"
+if (username === "admin" && password === "1234") {
+    console.log("Login Successful")
+}else{
+    console.log("Invalid username or password")
+}
+
+
+for ( let i = 1;i <= 20; i++) {
+console.log(i);
+}
+
+for ( let a = 2;a <= 50; a += 2 ){
+console.log(a);
+
+}
+for(let x = 1; x <= 25; x += 2){
+    console.log(x);
+}
+for (let y = 1; y <= 12; y++){
+    console.log(`7 * ${y} = ${7 * y}`
+}
+
+    
